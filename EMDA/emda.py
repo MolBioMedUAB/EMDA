34a8b6ffeb5a43709230e28ba621e7bec6e2fe4e
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2024-01-22 17:49:05 +0100

    Created the print_available_adders EMDA's method to print all the available adders with their Description and Usage section from docstrings. First steps on the run method.

--- EMDA/emda.py
+++ EMDA/emda.py
@@ -11,7 +11,9 @@ from adders import *
 # adders
 
 # load custom exceptions
-from exceptions import NotAvailableOptionError
+from exceptions import EmptyMeasuresError
+
+from tqdm.autonotebook import tqdm
 
 #from metaclass import add_adders
 #class EMDA(metaclass=add_adders):
@@ -22,7 +24,7 @@ class EMDA:
 
         self.parameters = parameters
         self.trajectory = trajectory
-        self._universe  = Universe(parameters, trajectory)
+        self.__universe  = Universe(parameters, trajectory)
         print('Trajectory has been loaded!')
         self.measures   = {}
         self.selections = {}
@@ -47,128 +49,93 @@ class EMDA:
         self.selections[name] = selection(self._universe, sel_input, sel_type=sel_type, no_backbone=no_backbone, return_atomic_sel_string=return_atomic_sel_string)
 
 
-    
-    
+    def print_available_adders(self, all_description=False):
 
-"""
-    # functions for creating Measures classes
-    def add_distance(self, name, sel1, sel2, type='min'):
-        to_add = adders.distance(name, self.selections[sel1], self.selections[sel2], type=type)
-
-        self.measures[name] = self.Measure(
-                name    = to_add['name'],
-                type    = to_add['type'],
-                sel     = to_add['sel'],
-                options = to_add['options'],
-                result  = []
-            )
+        import inspect
         
-
-    def add_angle(self, name, sel1, sel2, sel3, units='deg', domain=360):
-        to_add = adders.angle(name, self.selections[sel1], self.selections[sel2], self.selections[sel3], units=units, domain=domain)
-
-        self.measures[name] = self.Measure(
-                name    = to_add['name'],
-                type    = to_add['type'],
-                sel     = to_add['sel'],
-                options = to_add['options'],
-                result  = []
-            )
+        adder_methods = [method for method in dir(self) if method.startswith('add_')]
         
-    def add_dihedral(self, name, sel1, sel2, sel3, sel4, units='deg', domain=360):
-        to_add = adders.angle(name, self.selections[sel1], self.selections[sel2], self.selections[sel3], self.selections[sel4], units=units, domain=domain)
+        for method_name in adder_methods:
+            method = getattr(self, method_name)
 
-        self.measures[name] = self.Measure(
-                name    = to_add['name'],
-                type    = to_add['type'],
-                sel     = to_add['sel'],
-                options = to_add['options'],
-                result  = []
-            )
+            docstring = inspect.getdoc(method).split('\n')
 
-    def add_planar_angle(self, name, sel1, sel2, units='deg', domain=360):
-        to_add = adders.planar_angle(name, self.selections[sel1], self.selections[sel2], units=units, domain=domain)
+            # Extract DESCRIPTION and USAGE sections from docstring
+            description_section = ''
+            usage_section       = ''
+            for l in range(len(docstring)):
+                if 'DESCRIPTION' in docstring[l]:
+                    description_section += docstring[l]  + '\n'
+                    l_ = l
+                    while True:
+                        l_ += 1
+                        if docstring[l_] == '':
+                            break
+                        else :
+                            description_section += docstring[l_]  + '\n'
+                
+                if 'USAGE' in docstring[l]:
+                    usage_section += docstring[l] + '\n'
+                    l_ = l
+                    while True:
+                        l_ += 1
+                        if docstring[l_] == '':
+                            break
+                        else :
+                            usage_section += docstring[l_]  + '\n'
 
-        self.measures[name] = self.Measure(
-                name    = to_add['name'],
-                type    = to_add['type'],
-                sel     = to_add['sel'],
-                options = to_add['options'],
-                result  = []
-            )
+            if description_section == '':
+                description_section = 'No description available.\n'
+            if usage_section == '':
+                usage_section = 'No usage available.\n'
+
+            print(f"\033[1mMethod: {method_name}\033[0m\nHelp:\n{description_section}\n{usage_section}\n")
+
+        print("Use '>>> help(EMDA.add_***)' to get the complete information of an adder.")
 
 
-    def add_contacts(self, name, sel, sel_env, interactions="all", include_WAT=False, out_format='new', measure_distances=True):
+    def run(self, exclude=None, step=1, start=1, end=-1,):
+        """ 
+        DESCRIPTION:
+            Run all the measurements configured in self.measures. 
 
-        to_add = adders.contacts(self._universe, name, sel, sel_env, interactions=interactions, include_WAT=include_WAT, out_format=out_format, measure_distances=measure_distances)
+        OPTIONS:
+            - exclude:  skip measures with the given name 
+            - step:     Frames to jump during the analysis. Default is 1, so all the trajectory will be analysed.
+            - start:    First frame to start the analysis. Default is 0.
+            - end:      Last frame to analyse (included). Default is last frame of trajectory
+        """
+        
+        if len(self.measures) == 0:
+            raise EmptyMeasuresError
+        
+        if end == -1: end = len(self.__universe.trajectory) # so if no last frame given, last in trajectory is chosen.
 
-        self.measures[name] = self.Measure(
-            name    = to_add['name'],
-            type    = to_add['type'],
-            sel     = to_add['sel'],
-            options = to_add['options'],
-            result  = []
-        )
-
-
-    def add_RMSD(self, name, sel, ref=None, superposition=True):
-        to_add = adders.RMSD(self._universe, name, sel, ref=ref, superposition=superposition)
-
-        self.measures[name] = self.Measure(
-            name    = to_add['name'],
-            type    = to_add['type'],
-            sel     = to_add['sel'],
-            options = to_add['options'],
-            result  = []
-        )
-
-    def add_distWATbridge(self, name, sel1, sel2, sel1_rad=3, sel2_rad=3):
-        to_add = adders.distWATbridge(self._universe, name, sel1, sel2, sel1_rad=sel1_rad, sel2_rad=sel2_rad)
-
-        self.measures[name] = self.Measure(
-            name    = to_add['name'],
-            type    = to_add['type'],
-            sel     = to_add['sel'],
-            options = to_add['options'],
-            result  = []
-        )
-
-    def add_pKa(self, name, excluded_ions=["Na+", "Cl-"], pka_ref='neutral', pdb_folder='.pka', keep_pdb=False, keep_pka=False):
-        print('WARNING!!:')
-        print('  Take into account that pKa calculation with PROpKA requires')
-        print('  the generation of a PDB for each analysed frame.')
-        print('  This can be very time consuming for long trajectory, so consider')
-        print('  using the step option in run_measure() to reduce')
-        print('  the amount of analysed structures.')
+        # trajectory cycle
+        for ts in tqdm(self.__universe.trajectory[start, end, step], desc='Measuring', unit='Frame'):
+            # measures cycle
+            for measure in self.measures:
+                
 
 
 
-        to_add = adders.pKa(self._universe, name, excluded_ions=excluded_ions, pka_ref=pka_ref, pdb_folder=pdb_folder, keep_pdb=keep_pdb, keep_pka=keep_pka)
 
-        self.measures[name] = self.Measure(
-            name    = to_add['name'],
-            type    = to_add['type'],
-            sel     = to_add['sel'],
-            options = to_add['options'],
-            result  = []
-        )
+
+
+
+        pass
+
+    def analyse():
+        pass
+
     
-    ##### END ADDING AVAILABLE MEASURES #####
-
-    def remove_measurement(self, name):
-        if isinstance(name, str): name = [name] 
-
-        for measurement_index in range(len(self.measurements)):
-            for name_ in name:
-                if self.measurements[measurement_index]['name'] == name_:
-                    self.measurements.pop(measurement_index)
-"""
+    
 
 emda = EMDA('../example/parameters.prmtop', '../example/trajectory.nc')
-emda.select('first_resids', [1, 2, 3], sel_type='res_num')
-emda.select('second_resids', [4, 5, 6], sel_type='res_num')
+#emda.select('first_resids', [1, 2, 3], sel_type='res_num')
+#emda.select('second_resids', [4, 5, 6], sel_type='res_num')
 
-emda.add_distance(name='dist_first_second', sel1='first_resids', sel2='second_resids', type='min')
+#emda.add_distance(name='dist_first_second', sel1='first_resids', sel2='second_resids', type='min')
 
-print(emda.measures)
-print()
+print(emda.print_available_adders())
+
