a9324243c4a4e6f71948d5f216a3cf6578736d2b
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2024-01-25 12:32:41 +0100

    Version changed to 0.1.0

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
-#version='0.0.0'
-version='{{VERSION_PLACEHOLDER}}'
+version='0.1.0'
+#version='{{VERSION_PLACEHOLDER}}'
 
 setup(
     name="EMDA",
