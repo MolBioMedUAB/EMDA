ae5e0d0a297a350e80ad55d691571fc3f28ecce9
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2024-01-26 09:59:46 +0100

    Local update of setup.py for pulling

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
+version='0.1.0'
+#version='{{VERSION_PLACEHOLDER}}'
+
 setup(
     name="EMDA",
-    version='{{VERSION_PLACEHOLDER}}',
+    vesion=version,
     description="Easy MD Analysis (EMDA) is a Python package created with the aim of providing an easy, yet powerful way to perform analysis of MD simulations.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,5 +19,5 @@ setup(
     install_requires=["numpy", "pyyaml", "MDAnalysis", "tqdm"],
     keywords="biochemistry, simulations, MDAnalysis, molecular dynamics",
     url="https://github.com/MolBioMedUAB/EMDA",
-    download_url="https://github.com/MolBioMedUAB/EMDA/archive/refs/tags/{{VERSION_PLACEHOLDER}}.tar.gz",
+    download_url=f"https://github.com/MolBioMedUAB/EMDA/archive/refs/tags/{version}.tar.gz",
 )
