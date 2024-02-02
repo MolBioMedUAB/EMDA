d80d839e48e84ff69aae66e68cb9cc9dc1c485c7
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2024-02-02 16:19:18 +0100

    Added a function to identify if the code is being executed in a Notebook or not

--- EMDA/tools.py
+++ EMDA/tools.py
@@ -37,3 +37,20 @@ def read_analysis(self, analysis_filename, analysis=None):
 
     with open(analysis_filename, 'rb') as handle:
         self.analyses[analysis] = pickle.load(handle)
+
+
+def in_notebook() -> bool:
+    """
+    Source : 
+        https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+    """
+
+    try:
+        from IPython import get_ipython
+        if 'IPKernelApp' not in get_ipython().config:  # pragma: no cover
+            return False
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+    return True
