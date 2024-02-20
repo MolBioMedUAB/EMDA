4fe5ad9ee6db588214fa4df5da3e2d6761e2693e
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2024-02-20 13:58:32 +0100

    sel_env argument of calc_contacts_selection and calc_contacts_protein in run_contacts and run_protein_contacts, respectively, have been fixed and changed to a selection command

--- EMDA/runners.py
+++ EMDA/runners.py
@@ -21,8 +21,8 @@ def run_distance(self, Measure, variant, replica):
 
     Measure.result[variant][replica].append(
         calc_distance(
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[0])),
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[1])),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[1]]),
             Measure.options["type"])
     )
 
@@ -35,9 +35,9 @@ def run_angle(self, Measure, variant, replica):
 
     Measure.result[variant][replica].append(
         calc_angle(
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[0])),
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[1])),
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[2])),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[1]]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[2]]),
             Measure.options["units"],
             Measure.options["domain"],
         )
@@ -52,10 +52,10 @@ def run_dihedral(self, Measure, variant, replica):
 
     Measure.result[variant][replica].append(
         calc_dihedral(
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[0])),
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[1])),
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[2])),
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[3])),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[1]]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[2]]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[3]]),
             Measure.options["units"],
             Measure.options["domain"],
         )
@@ -70,8 +70,8 @@ def run_planar_angle(self, Measure, variant, replica):
 
     Measure.result[variant][replica].append(
         calc_planar_angle(
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[0])),
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[1])),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[1]]),
             Measure.options["units"],
             Measure.options["domain"],
         )
@@ -86,7 +86,7 @@ def run_pka(self, Measure, variant, replica):
 
     Measure.result[variant][replica].append(
         calc_pka(
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[0])),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]]),
             Measure.options["pka_ref"],
             Measure.options["pdb_folder"],
             Measure.options["keep_pdb"],
@@ -103,11 +103,13 @@ def run_contacts(self, Measure, variant, replica):
 
     Measure.result[variant][replica].append(
         calc_contacts_selection(
-            self.universe[variant][replica].select_atoms(self.selections(Measure.sel[0])),
-            self.universe[variant][replica].select_atoms(Measure.sel[0]),
+            self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]]),
+            self.universe[variant][replica].select_atoms(
+                f"around {Measure.sel[1]} group select",
+                select=self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]])
+            ),
             Measure.options["interactions"],
             Measure.options["measure_dists"],
-            Measure.options["out_format"],
             )
         )
 
@@ -117,11 +119,14 @@ def run_protein_contacts(self, Measure, variant, replica):
     DESCRIPTION:
 
     """
-    
+
     Measure.result[variant][replica].append(
         calc_contacts_protein(
             self.universe[variant][replica].select_atoms(Measure.sel[0]),
-            self.universe[variant][replica].select_atoms(Measure.sel[1]),
+            self.universe[variant][replica].select_atoms(
+                f"around {Measure.sel[1]} group select",
+                select=self.universe[variant][replica].select_atoms(self.selections[Measure.sel[0]])
+            ),
             Measure.options["interactions"],
             Measure.options["measure_dists"],
             Measure.options["out_format"],
