ce09d67b10a52782ccf4698b559a76bd238ab977
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2025-04-13 22:57:11 +0200

    Average function added to Measure dataclass

--- EMDA/emda.py
+++ EMDA/emda.py
@@ -244,12 +244,14 @@ class EMDA:
         def plot(self, same_y : bool = True, same_x : bool = True, axis_label_everywhere=False, combine_replicas=False, out_name=None):
             plot_measure(self, measure_name=None, same_y=same_y, same_x=same_x, axis_label_everywhere=axis_label_everywhere, combine_replicas=combine_replicas, out_name=out_name)
 
-        def average(self, measure_name=None, round_decimals=3, std=True):
+        def average(self, measure_name=None, round_decimals=3, std=True, return_data=False):
             """
             DESCRIPTION:
                 Calculates the average of the measure's results. It is not implemented yet.
             """
-            pass
+
+            average(self, measure_name=measure_name, round_decimals=round_decimals, std=std, return_data=return_data)
+            
 
     # create Analysis dataclass
     @dataclass
