dc4570f11d6c6e97d4add69dc558c5aa2e910fe5
Miquel Canyelles Niño <mcanyellesnino@gmail.com>
2025-05-04 21:24:36 +0200

    Trying to fix an error with bodies list

--- EMDA/analysers.py
+++ EMDA/analysers.py
@@ -1013,7 +1013,7 @@ def averager(self, measure_name, round_decimals=3, std=3, print_labels=True, ret
             if max_value_length < max([len(v) for v in bodies[n_var][1:]]):
                 max_value_length = max([len(v) for v in bodies[n_var][1:]])
 
-            bodies.append('\n')
+            bodies[n_var].append('\n')
 
         for var in bodies:
             body += var[0]
